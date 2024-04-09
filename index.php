87fce3d3caa8167cf7994fac3e521131074a1ec0
Vadim Malashenko <zex404@gmail.com>
2024-04-10 08:32:05 +1000

    Move to local container.

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+    <head>
+
+        <title>Coffee Shop</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="/assets/styles/main.css">
+        
+    </head>
+
+    <body>
+
+        <header>
+
+            <h1>Выбор напитка</h1>
+        
+            <nav>
+                <a href="#phone">T</a>
+                <a href="#login">Вход</a>
+                <a href="#register">Регистрация</a>
+            </nav>
+        
+        </header>
+        
+        <nav>
+        
+            <ul>
+        
+                <li>
+                    <a href="#coffee" title="Бодрящий кофе">
+                        <img src="https://raw.githubusercontent.com/vadim-malashenko/coffee-shop/main/assets/images/coffee.png" alt="Чашка эспрессо">
+                        <p>Кофе</p>
+                    </a>
+                </li>
+        
+                <li>
+                    <a href="#tea" title="Ароматный чай">
+                        <img src="https://raw.githubusercontent.com/vadim-malashenko/coffee-shop/main/assets/images/tea.png" alt="Чай">
+                        <p>Чай</p>
+                    </a>
+                </li>
+        
+                <li>
+                    <a href="#milkshake" title="Пломбир и отборное молоко">
+                        <img src="https://raw.githubusercontent.com/vadim-malashenko/coffee-shop/main/assets/images/milkshake.png" alt="Молочный коктейль">
+                        <p>Коктейли</p>
+                    </a>
+                </li>
+        
+                <li>
+                    <a href="#fruit" title="Фруктовая сладость">
+                        <img src="https://raw.githubusercontent.com/vadim-malashenko/coffee-shop/main/assets/images/fruit.png" alt="Фруктовый напиток">
+                        <p>Морсы</p>
+                    </a>
+                </li>
+        
+            </ul>
+        
+        </nav>
+
+        <article id="coffee">
+
+            <header>        
+                <h1>Кофе</h1>
+            </header>
+        
+            <article>
+                
+                <a href="#cup" title="Эспрессо">
+                    <img src="https://raw.githubusercontent.com/vadim-malashenko/coffee-shop/main/assets/images/coffee/espresso.jpg" alt="Чашка эспрессо">
+                    <h1>Эспрессо</h1>
+                    <p>от <span>79₽</span></p>
+                </a>
+
+            </article>
+
+            <article>
+                
+                <a href="#cup" title="Эспрессо 2x">
+                    <img src="https://raw.githubusercontent.com/vadim-malashenko/coffee-shop/main/assets/images/coffee/espresso.jpg" alt="Чашка эспрессо">
+                    <h1>Эспрессо 2x</h1>
+                    <p>от <span>109₽</span></p>
+                </a>
+
+            </article>
+
+            <article>
+                
+                <a href="#cup" title="Американо">
+                    <img src="https://raw.githubusercontent.com/vadim-malashenko/coffee-shop/main/assets/images/coffee/americano.jpg" alt="Чашка американо">
+                    <h1>Американо</h1>
+                    <p>от <span>119₽</span></p>
+                </a>
+
+            </article>
+        
+            <article>
+                
+                <a href="#cup" title="Латте">
+                    <img src="https://raw.githubusercontent.com/vadim-malashenko/coffee-shop/main/assets/images/coffee/latte.jpg" alt="Чашка латте">
+                    <h1>Латте</h1>
+                    <p>от <span>129₽</span></p>
+                </a>
+
+            </article>
+
+        </article>
+
+    </body>
+
+</html>
