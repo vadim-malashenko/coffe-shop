f07eb72b3112ab5f1768ccfe06e6f1884d70866f
Vadim Malashenko <zex404@gmail.com>
2024-04-16 20:55:25 +1000

    Fix index.json

--- docs/assets/scripts/index.js
+++ docs/assets/scripts/index.js
@@ -1,13 +1,74 @@
-class App
+class Http
 {
+    async get(url)
+    {
+        const request = await fetch(url)
+
+        const status = request.status
+        const body = 200 === request.status
+            ? await request.json()
+            : request.statusText
+
+        return {status, body}
+    }
+}
+
+class Event extends EventTarget
+{
+    emit(type, detail)
+    {
+        this.dispatchEvent(Event.create(type, detail))
+    }
+
+    on(type, handler)
+    {
+        this.addEventListener(type, handler.bind(this))
+    }
+
+    off(type, handler)
+    {
+        this.removedEventListener(type, handler.bind(this))
+    }
+
+    static create(type, detail)
+    {
+        return new CustomEvent(type, {detail})
+    }
+}
+
+class Menu extends Event
+{
+    #root
+
+    constructor(selector, data)
+    {
+        this.#root = document.querySelector(selector)
+        this.#root.insertAjacentHTML(`beforeend`, this.render(data))
+    }
+
+    render(data)
+    {
+        return `<menu>`
+            + data.map(item => `<li><img src="/coffee-shop/docs/assets/images/${item.src}"></li>`)
+        `</menu>`
+    }
+}
+
+class App extends Http
+{
+    #menu
+
+    constructor(menu)
+    {
+        this.#menu = menu
+    }
+
     static async load(ev)
     {
-        const request = await fetch(`/coffee-shop/docs/assets/data/drinks.json`)
-        const response = await request.json()
-        
-        const menu = response
+        const menu = await get(`/coffee-shop/docs/assets/data/drinks.json`)
+        const app = new App(new Menu(`body`, menu))
 
-        console.log(menu)
+        console.log(app)
     }
 }
 
